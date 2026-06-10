09f447530aab3c7d68e88955c36c967783534b16
as2305 <165639720+as2305@users.noreply.github.com>
2026-06-10 15:13:09 +0530

    Init fastapi & get balance & history request

--- database.py
+++ database.py
@@ -2,13 +2,14 @@ import json
 from pathlib import Path
 
 balance = {
-    "balance": 0
+    "balance": 200
     }
 transaction_history = {}
 
 bal_path = Path("balance.json")
 th_path = Path("transaction_history.json")
 
+#Initialize Database
 def init_db():
     global balance
     global transaction_history
@@ -21,8 +22,8 @@ def init_db():
     else:
         transaction_history = json.loads(th_path.read_text())
 
+#Update JSON
 def update_bal():
     bal_path.write_text(json.dumps(balance, indent = 4))
-
 def update_th():
     th_path.write_text(json.dumps(transaction_history, indent = 4))
--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from fastapi import FastAPI
+import database
+
 
 app = FastAPI()
 
+database.init_db()
+
+
+@app.get("/balance")
+def get_balance():
+    return database.balance
+
+@app.get("/history")
+def get_history():
+    return database.history
