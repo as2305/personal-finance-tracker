3b52fb15dc752feab2cc01f31c17c408c2c1bb03
as2305 <165639720+as2305@users.noreply.github.com>
2026-06-10 16:19:24 +0530

    Create add transction function & record POST request function

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
 import json
 from pathlib import Path
+from datetime import date
 
 balance = {
-    "balance": 200
+    "balance": 0
     }
-transaction_history = {}
+transaction_history = []
 
 bal_path = Path("balance.json")
 th_path = Path("transaction_history.json")
@@ -27,3 +28,22 @@ def update_bal():
     bal_path.write_text(json.dumps(balance, indent = 4))
 def update_th():
     th_path.write_text(json.dumps(transaction_history, indent = 4))
+
+#add transaction
+def add_transaction(mode, amount):
+    global balance
+    global transaction_history
+    if mode == "deposit":
+        balance["balance"] += amount
+        transaction_history.append({"mode": mode, "amount": amount, "date": str(date.today())})
+        update_bal()
+        update_th()
+        return True
+    elif mode == "withdraw":
+        balance["balance"] -= amount
+        transaction_history.append({"mode": mode, "amount": amount, "date": str(date.today())})
+        update_bal()
+        update_th()
+        return True
+    else:
+        return False
--- main.py
+++ main.py
@@ -7,10 +7,19 @@ app = FastAPI()
 database.init_db()
 
 
+@app.post("/record")
+def record(mode:str, amount:int):
+    output = database.add_transaction(mode, amount)
+    if output:
+        return {"message": "success"}
+    else:
+        return {"message": "failed"}
+    
+
 @app.get("/balance")
 def get_balance():
     return database.balance
 
 @app.get("/history")
 def get_history():
-    return database.history
+    return database.transaction_history
