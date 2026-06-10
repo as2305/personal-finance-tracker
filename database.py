6a36a07704b933438c053509a363ea02f94df65c
as2305 <165639720+as2305@users.noreply.github.com>
2026-06-10 14:56:03 +0530

    Add files & Initialize

---
+++ database.py
@@ -0,0 +1,28 @@
+import json
+from pathlib import Path
+
+balance = {
+    "balance": 0
+    }
+transaction_history = {}
+
+bal_path = Path("balance.json")
+th_path = Path("transaction_history.json")
+
+def init_db():
+    global balance
+    global transaction_history
+    if not bal_path.is_file():
+        bal_path.write_text(json.dumps(balance, indent = 4))
+    else:
+        balance = json.loads(bal_path.read_text())
+    if not th_path.is_file():
+        th_path.write_text(json.dumps(transaction_history, indent = 4))
+    else:
+        transaction_history = json.loads(th_path.read_text())
+
+def update_bal():
+    bal_path.write_text(json.dumps(balance, indent = 4))
+
+def update_th():
+    th_path.write_text(json.dumps(transaction_history, indent = 4))
